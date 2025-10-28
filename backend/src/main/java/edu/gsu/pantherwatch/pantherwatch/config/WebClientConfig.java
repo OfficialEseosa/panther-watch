874876b41c93d1e80b9f31ac03d48531a883c5c3
Raphael Omorose <raphaelomorose@gmail.com>
2025-10-28 09:39:37 -0400

    Revert "Merge branch 'main' of https://github.com/OfficialEseosa/panther-watch"
    This reverts commit efff43ff877d74c0af6bf15dae82f359a1da507d, reversing
    changes made to 4713dd82d891505d942a62b9fed221234a492288.

--- backend/src/main/java/edu/gsu/pantherwatch/pantherwatch/config/WebClientConfig.java
+++ backend/src/main/java/edu/gsu/pantherwatch/pantherwatch/config/WebClientConfig.java
@@ -7,24 +7,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.http.HttpHeaders;
 
-import java.net.URI;
-
 @Configuration
 public class WebClientConfig {
     public static final String ACCEPT = "application/json, text/javascript, */*; q=0.01";
     public static final String X_REQUESTED_WITH = "XMLHttpRequest";
-    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-    private static final String CLASS_SEARCH_PATH = "/classSearch/classSearch";
 
     @Value("${pantherwatch.api.base-url}")
     private String baseUrl;
 
     @Bean
     public WebClient webClient() {
-        URI baseUri = URI.create(baseUrl);
-        String origin = baseUri.getScheme() + "://" + baseUri.getHost();
-        String referer = baseUrl + CLASS_SEARCH_PATH;
-
         ExchangeStrategies strategies = ExchangeStrategies.builder()
                 .codecs(configurer -> configurer
                         .defaultCodecs()
@@ -36,9 +28,6 @@ public class WebClientConfig {
                 .exchangeStrategies(strategies)
                 .defaultHeader(HttpHeaders.ACCEPT, ACCEPT)
                 .defaultHeader("X-Requested-With", X_REQUESTED_WITH)
-                .defaultHeader(HttpHeaders.REFERER, referer)
-                .defaultHeader(HttpHeaders.ORIGIN, origin)
-                .defaultHeader(HttpHeaders.USER_AGENT, USER_AGENT)
                 .build();
     }
 }
