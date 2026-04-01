2c5f6df9feca26c2e2c3ff2a5cd7ca06b7ba7ae5
Raphael Omorose <raphaelomorose@gmail.com>
2026-04-01 10:42:54 -0400

    refactor: Remove hardcoded CORS allowed origins and use allowed origin patterns in WebMvcConfig

--- backend/src/main/java/edu/gsu/pantherwatch/pantherwatch/config/WebMvcConfig.java
+++ backend/src/main/java/edu/gsu/pantherwatch/pantherwatch/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package edu.gsu.pantherwatch.pantherwatch.config;
 
 import edu.gsu.pantherwatch.pantherwatch.security.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,9 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private AuthenticationInterceptor authenticationInterceptor;
 
-    @Value("${pantherwatch.cors.allowed-origins:http://localhost:5173}")
-    private String allowedOrigins;
-
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
@@ -28,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
